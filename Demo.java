c2a2a291d1acfeeb9d772f296bbda6c3b6c112ce
Maha <cumaha@gmail.com>
2017-11-04 22:27:45 -0400

    modified SystemOut print statememt

--- Demo.java
+++ Demo.java
@@ -2,7 +2,8 @@ public class Demo {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello.. This is test.java");
+        System.out.println("Hello.. This is modified");
+		//This is to test the file
     }
 
 }
