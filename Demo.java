9d906e3c897988bbb63d84f480f97e3ea3247904
cumaha <cumaha@gmail.com>
2017-11-07 19:30:35 -0500

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello.. This is modified");
+        System.out.println("Hello.. This is Demo");
 		//This is to test the file
     }
 
